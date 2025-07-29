0b68e0c34bdcdee751a8e81bf9c8f651ca8effea
Neo <pytandgit@gmail.com>
2025-07-29 10:31:21 +0300

    Создаем главное окно проекта и метку и кнопку

---
+++ Dogs.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+
+
+
+
+
+window = Tk()
+window.title('Картинка с собачками')
+window.geometry('360x420')
+label = Label()
+label.pack(pady=10)
+
+button = Button(text='Загрузить изображение', command=show_image)
+button.pack(pady=10)
+
+
+
+
+
+
+window.mainloop()
